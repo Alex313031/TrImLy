1482cc7851e33b3d9e740d3b100c44b0b2c2a576
Alexander David Frick <Alex313031@gmail.com>
2021-06-16 17:43:11 -0500

    Update thank yous, cleanup comments.

--- trimly.sh
+++ trimly.sh
@@ -13,8 +13,8 @@ printf "${RED}==TrImLy - Your fstrim automator and defrag script for ChomiumOS==
 printf "\n"
 printf "Note: This script must be made placed in /usr/local/bin,\n made executable with 'sudo chmod +x trimly.sh',\n and run with sudo (root) priveleges.\n"
 printf "\n"
-# conditional defrag (will not work entirely on bind mounts used on /)
 
+# conditional defrag (*will not work entirely on bind mounts used on /)
 printf "You may optionally run e4defrag -v on the stateful_partition and root (/), note this will take some time, and\n is not needed very often on SSDs, but one should use this if installed to an HDD, as ChromiumOS has no auto-defrag. \nPlease answer 1 or 2.▼\n"
 printf "\n"
 	select yn in "Yes" "No"; do
@@ -29,12 +29,12 @@ printf "${YEL}==TRIMming of SSD...==${NC}\n"
 printf "\n"
 
 # run fstrim on all relevant partitions and mount points.
-
 fstrim -v /mnt/stateful_partition/encrypted
 fstrim -v /usr/share/oem
 fstrim -v /mnt/stateful_partition
 fstrim -v /
-# mount rootfs to temp dir and fstrim them
+
+# mount rootfs to temp dir and fstrim them.
 mkdir /tmp_mnt
 mount /dev/sda18 /tmp_mnt
 fstrim -v /dev/sda18
@@ -42,7 +42,7 @@ umount /tmp_mnt
 mount /dev/sda20 /tmp_mnt
 fstrim -v /tmp_mnt
 umount /dev/sda20
-# exit 
+# exit
 printf "\n"
 printf "Done.\n"
 printf "\n"
