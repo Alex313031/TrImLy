045dec72dbef7c75418b9dc854a0ea83bdf1f923
Alexander David Frick <Alex313031@gmail.com>
2021-06-19 11:23:15 -0500

    Update Trimly to 0.5
    Updated colours, options, conditional defrag/trim, comments, and refactoring/cleaning up.

--- trimly.sh
+++ trimly.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 
+## TrImLy
 # trimly.sh
+## VER. 0.5
 
-printf "\n"
-
-# constants
-RED='\033[0;31m'
-YEL='\033[1;33m'
+# constants (for coloring stdout)
+RED='\033[0;31m' # Red
+YEL='\033[1;33m' # Yellow
 NC='\033[0m' # No Color
 
-printf "${RED}==TrImLy - Your fstrim automator and defrag script for ChomiumOS==${NC}\n"
+#start
+printf "${RED}--------------------------------------------------------------------------------${NC}\n"
+printf "\n"
+
+printf "${RED}== TrImLy - Your fstrim automator and defrag script for ChomiumOS ==${NC}\n"
 printf "\n"
 printf "Note: This script must be made placed in /usr/local/bin,\n made executable with 'sudo chmod +x trimly.sh',\n and run with sudo (root) priveleges.\n"
 printf "\n"
 
 # conditional defrag (*will not work entirely on bind mounts used on /)
-printf "You may optionally run e4defrag -v on the stateful_partition and root (/), note this will take some time, and\n is not needed very often on SSDs, but one should use this if installed to an HDD, as ChromiumOS has no auto-defrag. \nPlease answer 1 or 2.▼\n"
+printf "${YEL}You may optionally run e4defrag -v on the stateful_partition and root (/), note this will take some time, and\n is not needed very often on SSDs, but one should use this if installed to an HDD, as ChromiumOS has no auto-defrag. \nPlease answer 1 or 2.▼${NC}\n"
 printf "\n"
 	select yn in "Yes" "No"; do
     case $yn in
-    	Yes ) e4defrag -v /dev/sda16; echo "Continuing..."; break;;
-        No ) echo "Continuing..."; break;;
+    	Yes ) printf "\n" && printf "${YEL}Defragmenting HDD...${NC}" && printf "\n" && e4defrag -v /var && e4defrag -v /var && printf "\n ${RED}Continuing...${NC}" && printf "\n"; break;;
+        No ) printf "\n ${RED}Continuing...${NC}" && printf "\n"; break;;
     esac
 done
 
+# conditional trim (for SSD performance and longevity)
+# -run fstrim on all relevant partitions and mount points.
+# -mount rootfs to temp dir and fstrim them.
+printf "\n${YEL}You may now optionally run fstrim on all relevant partitions, mount points,\n and the two rootfs partitions A and B. Note that the time for completion\n depends on SSD size/speed, and the amount of deletions made since last trim. \nPlease answer 1 or 2.▼${NC}\n"
 printf "\n"
-printf "${YEL}==TRIMming of SSD...==${NC}\n"
-printf "\n"
+	select yn in "Yes" "No"; do
+    case $yn in
+    	Yes ) printf "\n" && printf "${YEL}TRIMming SSD...${NC}\n" && fstrim -v /mnt/stateful_partition/encrypted && fstrim -v /usr/share/oem && fstrim -v /mnt/stateful_partition && fstrim -v / && mkdir /rootfs_A && mount /dev/sda3 /rootfs_A && fstrim -v /rootfs_A && umount /dev/sda3 && mkdir /rootfs_B && mount /dev/sda5 /rootfs_B && fstrim -v /rootfs_B && umount /dev/sda5 && printf "fstrim -v completed successfully with status code 0" && printf "\n"; break;;
+        No ) printf "\n ${RED}Skipping TRIM...${NC}" && printf "\n"; break;;
+    esac
+done
 
-# run fstrim on all relevant partitions and mount points.
-fstrim -v /mnt/stateful_partition/encrypted
-fstrim -v /usr/share/oem
-fstrim -v /mnt/stateful_partition
-fstrim -v /
-
-# mount rootfs to temp dir and fstrim them.
-mkdir /tmp_mnt
-mount /dev/sda18 /tmp_mnt
-fstrim -v /dev/sda18
-umount /tmp_mnt
-mount /dev/sda20 /tmp_mnt
-fstrim -v /tmp_mnt
-umount /dev/sda20
-# exit
+# cleanup and exit
 printf "\n"
-printf "Done.\n"
-printf "\n"
-printf "${YEL}TrImLy has optimized your storage device(s) for faster I/O operations!${NC} *yay*\n"
-printf "\n"
+printf "\n ${RED}Done!${NC}\n"
+printf "\n-Cleaning up temporary rootfs mountpoints... (if you used TRIM)\n"
+rmdir /rootfs_A
+rmdir /rootfs_B
+printf "\n${YEL}TrImLy has optimized your storage device for faster I/O operations!${NC} *yay*\n"
+printf "${RED}--------------------------------------------------------------------------------${NC}\n"
 exit
 
 # Thanks to Ted Larson for motivating me to make this, my fellow nerd dad for too much to list here, and JCloud for fun chitchat.
